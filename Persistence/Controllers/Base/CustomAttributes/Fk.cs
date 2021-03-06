33ab9cab3728f539b1ee4d9c465b76c7394fac98
gvzero3 <veltenguilherme@gmail.com>
2021-03-06 15:37:34 -0300

    Alteração em permissões de acessos.

--- Persistence/Controllers/Base/CustomAttributes/Fk.cs
+++ Persistence/Controllers/Base/CustomAttributes/Fk.cs
@@ -4,13 +4,13 @@ using System;
 namespace Persistence.Controllers.Base.CustomAttributes
 {
     [AttributeUsage(AttributeTargets.Property)]
-    internal class Fk : Attribute
+    public class Fk : Attribute
     {
         internal string ColumnName { get; set; }
         internal string TableName { get; set; }
         internal FkType Type { get; set; }
 
-        internal Fk(string tableName, string columnName, FkType type)
+        public Fk(string tableName, string columnName, FkType type)
         {
             Type = type;
             TableName = tableName;
