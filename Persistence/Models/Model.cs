1496521a055007dfbccb0b798d93869245287bc9
g-velten <g-velten@relatar.com.br>
2023-01-13 00:05:03 -0300

    v. 2.0.3

--- Persistence/Models/Model.cs
+++ Persistence/Models/Model.cs
@@ -1,21 +1,25 @@
 ﻿using Persistence.Controllers.Base.CustomAttributes;
 using System;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Persistence.Models
 {
     public class Model<T> : IModel<T>
     {
+        [DefaultValue("00000000-0000-0000-0000-000000000000")] //swagger
         [System.ComponentModel.DataAnnotations.Key]
         [Column("uuid")]
         [SqlType(SqlTypes.GUID)]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public Guid? Guid { get; set; }
 
+        [DefaultValue(null)]
         [Column("inserted")]
         [SqlType(SqlTypes.TIMESTAMP_WITHOUT_TIME_ZONE_NOT_NULL)]
         public DateTime? Inserted { get; set; }
 
+        [DefaultValue(null)]
         [Column("updated")]
         [SqlType(SqlTypes.TIMESTAMP_WITHOUT_TIME_ZONE)]
         public DateTime? Updated { get; set; }
